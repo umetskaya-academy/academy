38834b6983512fd6170ae3e38717c554d073880b
Admin <Admin@DESKTOP-VMHEPPU>
2020-04-08 21:54:51 +0300

    pflfybt 1

---
+++ academy/src/by/academy/lesson3/Cycle1.java
@@ -0,0 +1,10 @@
+package by.academy.lesson3;
+
+public class Cycle1 {
+
+	
+
+	//1.Составьте программу, выводящую на экран квадраты чисел от 10 до 20..
+	public static void main(String[] args) {
+	
+}
