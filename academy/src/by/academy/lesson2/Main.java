509dc76875d8dced69a5a7d30131a7e0fc2d73f1
Admin <Admin@DESKTOP-VMHEPPU>
2020-04-08 16:52:18 +0300

    “Second commit!”

--- academy/src/by/academy/lesson2/Main.java
+++ academy/src/by/academy/lesson2/Main.java
@@ -1,7 +1,10 @@
 package by.academy.lesson2;
 
 public class Main {
-	public static void main(String... strings) {
-		System.out.println("Second commit");
-	}
-}
+	
+		public static void main(String[] args) {
+		      System.out.println("Second commit!"); 
+		   }
+		}
+
+
