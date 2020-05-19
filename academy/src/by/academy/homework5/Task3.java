4eb9a3083d1b101ec1392355f68af447394d6cbe
Admin <Admin@DESKTOP-VMHEPPU>
2020-05-19 13:48:37 +0300

    Homework 5. New Task 3.

--- academy/src/by/academy/homework5/Task3.java
+++ academy/src/by/academy/homework5/Task3.java
@@ -10,55 +10,37 @@ import java.util.NoSuchElementException;
 
 public class Task3<T> implements Iterator<T> {
 	private T[][] array;
+	private int i, j;
 
 	public Task3(T[][] array) {
 		this.array = array;
 	}
 
 	@Override
-	public Iterator<T> iterator() {
-		return new Iterator<T>() {
-
-			private int i, j;
-
-			@Override
-			public boolean hasNext() {
-				for (int i = this.i; i < array.length; i++) {
-					for (int j = this.j; j < array[i].length; j++) {
-						return true;
-					}
-				}
-				return false;
-			}
-
-			@Override
-			public T next() {
-				if (!hasNext())
-					throw new NoSuchElementException();
-				T t = array[i][j];
-				j++;
-				for (int i = this.i; i < array.length; i++) {
-					for (int j = (i == this.i ? this.j : 0); j < array[i].length; j++) {
-						this.i = i;
-						this.j = j;
-						return t;
-					}
-				}
-
-				return t;
-			}
-		};
-	}
-
-	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
+		for (int i = this.i; i < array.length; i++) {
+			for (int j = this.j; j < array[i].length;) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public T next() {
-		// TODO Auto-generated method stub
-		return null;
+		if (!hasNext())
+			throw new NoSuchElementException();
+		T t = array[i][j];
+		j++;
+		for (int i = this.i; i < array.length; i++) {
+			for (int j = (i == this.i ? this.j : 0); j < array[i].length;) {
+				this.i = i;
+				this.j = j;
+				return t;
+			}
+		}
+
+		return t;
 	}
+
 }
